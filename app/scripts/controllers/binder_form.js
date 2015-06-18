a2b9c3691b223ea5a39a0e36393dd8272f6377f8
Fabian Vogler <fabian@equivalence.ch>
2015-06-18 18:32:47 +0200

    Refactor controllers

--- app/scripts/controllers/binder_form.js
+++ app/scripts/controllers/binder_form.js
@@ -1,40 +1,39 @@
 'use strict';
 
-angular.module('skillMgmtApp')
-  .controller('BinderFormCtrl', function ($scope, $rootScope, $state, $stateParams, $translate, alert, Restangular) {
+angular.module('skillMgmtApp').controller('BinderFormCtrl', function ($scope, $rootScope, $state, $stateParams) {
   
-	  if ($stateParams.id == 1)
-	  {
-		  $scope.binderForm = $rootScope.form1;
-	  }
-	  else if ($stateParams.id == 2)
-	  {
-		  $scope.binderForm = $rootScope.form2;
-	  }
-	  else
-	  {
-		  $state.go("binder_form_list");
-	  }
-	  $scope.experts = $rootScope.experts;
-	  $scope.ce = $rootScope.ce;
-	  $scope.jp = $rootScope.jp;
-	  
-	  $scope.labelClass = function(type) {
-		  console.log(type);
-		  if (type == 'TEXT') {
-			  return 'col-sm-12';
-		  }
-		  else if (type == 'CE_APPROVAL') {
-			  return '';
-		  }
-		  else {
-			  return 'col-sm-12';
-		  }
-	  };
-	  
-	  $scope.submit = function () {
-	      $scope.formList[$stateParams.id - 1].state = 'submitted';
-	      $state.go('binder_form_list');
-	  };
-	  
-  });
+      if ($stateParams.id == 1)
+      {
+          $scope.binderForm = $rootScope.form1;
+      }
+      else if ($stateParams.id == 2)
+      {
+          $scope.binderForm = $rootScope.form2;
+      }
+      else
+      {
+          $state.go("binder_form_list");
+      }
+      $scope.experts = $rootScope.experts;
+      $scope.ce = $rootScope.ce;
+      $scope.jp = $rootScope.jp;
+      
+      $scope.labelClass = function(type) {
+          console.log(type);
+          if (type == 'TEXT') {
+              return 'col-sm-12';
+          }
+          else if (type == 'CE_APPROVAL') {
+              return '';
+          }
+          else {
+              return 'col-sm-12';
+          }
+      };
+      
+      $scope.submit = function () {
+          $scope.formList[$stateParams.id - 1].state = 'submitted';
+          $state.go('binder_form_list');
+      };
+      
+});
--- app/scripts/controllers/binder_form_list.js
+++ app/scripts/controllers/binder_form_list.js
@@ -1,10 +1,9 @@
 'use strict';
 
-angular.module('skillMgmtApp')
-  .controller('BinderFormListCtrl', function ($scope, $rootScope, $stateParams, $translate, alert, Restangular) {
-  
-	  $scope.formList = $rootScope.formList;
-	  $scope.experts = $rootScope.experts;
-	  $scope.ce = $rootScope.ce;
-	  $scope.jp = $rootScope.jp;
-  });
+angular.module('skillMgmtApp').controller('BinderFormListCtrl', function ($scope, $rootScope) {
+
+        $scope.formList = $rootScope.formList;
+        $scope.experts = $rootScope.experts;
+        $scope.ce = $rootScope.ce;
+        $scope.jp = $rootScope.jp;
+});
