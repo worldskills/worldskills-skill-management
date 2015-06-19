3920c9f737ee7dcd7f94fdf4ca8283e96e9e9074
Fabian Vogler <fabian@equivalence.ch>
2015-06-19 10:01:34 +0200

    Load forms

--- app/scripts/controllers/form_submission_list.js
+++ app/scripts/controllers/form_submission_list.js
@@ -1,12 +1,14 @@
 'use strict';
 
-angular.module('skillMgmtApp').controller('FormSubmissionListCtrl', function ($scope, $rootScope, auth, PersonSkills) {
+angular.module('skillMgmtApp').controller('FormSubmissionListCtrl', function ($scope, $rootScope, auth, PersonSkills, EventForms) {
 
     var eventId = 10;
 
     auth.user.$promise.then(function () {
         $scope.skills = PersonSkills.get({eventId: eventId, personId: auth.user.person_id});
     });
+
+    $scope.forms = EventForms.get({eventId: eventId});
 
         $scope.formList = $rootScope.formList;
         $scope.experts = $rootScope.experts;
