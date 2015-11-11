4a36524e979f889b23d972683e6eacd8b8d8714d
Fabian Vogler <fabian@equivalence.ch>
2015-11-11 15:15:12 +0100

    Use port 11200 for dev

--- Gruntfile.js
+++ Gruntfile.js
@@ -66,7 +66,7 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9000,
+        port: 11200,
         // Change this to '0.0.0.0' to access the server from outside.
         //hostname: 'localhost',
         hostname: '0.0.0.0',
