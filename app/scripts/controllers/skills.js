204e3656a89e7a13a28cadad7e58a471d97941d0
Fabian Vogler <fabian@equivalence.ch>
2021-02-12 09:34:05 +0100

    Add ViewManagementPlan permission check

--- app/scripts/controllers/skills.js
+++ app/scripts/controllers/skills.js
@@ -4,6 +4,12 @@ angular.module('skillMgmtApp').controller('SkillCtrl', function($scope, $statePa
 
     $scope.skill = Skill.get({id: $stateParams.skillId}, {}, function () {
 
+        auth.hasUserRole(WORLDSKILLS_API_SKILLMAN_CODE, 'ViewManagementPlan', $scope.skill.entity_id).then(function (hasUserRole) {
+            if (hasUserRole) {
+                $scope.userCanViewManagementPlan = true;
+            }
+        });
+
         auth.hasUserRole(WORLDSKILLS_API_SKILLMAN_CODE, 'ViewTimetable', $scope.skill.entity_id).then(function (hasUserRole) {
             if (hasUserRole) {
                 $scope.userCanViewTimetable = true;
