4722b6b8773ceb80450cb00dc3130d02ded4956f
Fabian Vogler <fabian@equivalence.ch>
2021-02-15 10:24:31 +0100

    Add correct link to IL

--- app/scripts/controllers/skills.js
+++ app/scripts/controllers/skills.js
@@ -1,8 +1,10 @@
 'use strict';
 
-angular.module('skillMgmtApp').controller('SkillCtrl', function($scope, $stateParams, WORLDSKILLS_API_SKILLMAN_CODE, auth, Skill) {
+angular.module('skillMgmtApp').controller('SkillCtrl', function($scope, $stateParams, $q, WorldSkills, WORLDSKILLS_API_SKILLMAN_CODE, WORLDSKILLS_WEB_PROTOCOL, WORLDSKILLS_WEB_DOMAIN, auth, Event, Skill, Resource) {
 
     $scope.skill = Skill.get({id: $stateParams.skillId}, {}, function () {
+    $scope.WORLDSKILLS_WEB_PROTOCOL = WORLDSKILLS_WEB_PROTOCOL;
+    $scope.WORLDSKILLS_WEB_DOMAIN = WORLDSKILLS_WEB_DOMAIN;
 
         auth.hasUserRole(WORLDSKILLS_API_SKILLMAN_CODE, 'ViewManagementPlan', $scope.skill.entity_id).then(function (hasUserRole) {
             if (hasUserRole) {
