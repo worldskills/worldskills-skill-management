003f2247df37e2408c2f265f78e0b0953dfd11ad
Fabian Vogler <fabian@equivalence.ch>
2023-10-26 13:58:03 +0200

    Use localhost for development

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,8 +64,7 @@ module.exports = function (grunt) {
       options: {
         port: 11200,
         // Change this to '0.0.0.0' to access the server from outside.
-        //hostname: 'localhost',
-        hostname: '0.0.0.0',
+        hostname: 'localhost',
         livereload: 35729
       },
       livereload: {
