68102ad8cd55b5750409043813dcda39c0d7fe1c
Fabian Vogler <fabian@equivalence.ch>
2025-05-15 20:57:42 +0200

    Add SMT election results

--- app/scripts/controllers/admin_skill.js
+++ app/scripts/controllers/admin_skill.js
@@ -33,7 +33,7 @@ angular.module('skillMgmtApp').controller('AdminSkillExpertsCtrl', function($sco
 
 });
 
-angular.module('skillMgmtApp').controller('AdminSkillExpertNominationsCtrl', function($scope, $stateParams, $state, $translate, alert, SkillExpert, PeoplePerson, Poll) {
+angular.module('skillMgmtApp').controller('AdminSkillExpertNominationsCtrl', function($scope, $stateParams, $state, $translate, alert, SkillExpert, PeoplePerson, SMTElectionVote) {
 
     $scope.loading = true;
     $scope.createPollLoading = false;
@@ -50,6 +50,18 @@ angular.module('skillMgmtApp').controller('AdminSkillExpertNominationsCtrl', fun
                 });
             });
         });
+
+
+        $scope.votes = SMTElectionVote.query({skillId: $stateParams.skillId}, function () {
+            $scope.votes.votes.forEach(function (vote) {
+                angular.forEach($scope.experts.registration_people, function (expert) {
+                    if (vote.voter.id === expert.person.id) {
+                        expert.vote = vote;
+                    }
+                });
+            });
+        });
+
         $scope.loading = false;
     });
 
@@ -70,69 +82,13 @@ angular.module('skillMgmtApp').controller('AdminSkillExpertNominationsCtrl', fun
     };
 
     $scope.createPoll = function () {
-
-        $scope.createPollLoading = true;
-
-        var numberOfSelections = Math.min(3, $scope.getNominatedTotal());
-
-        var experts = [];
-        var options = [];
-        var ces = [];
-        angular.forEach($scope.experts.registration_people, function (expert) {
-            experts.push(expert.person);
-            if (expert.nominated_smt) {
-                var name = expert.person.first_name + ' ' + expert.person.last_name;
-                if (expert.member) {
-                    name += ' ' + expert.member.code;
-                }
-                options.push({
-                    'text': {
-                        'lang_code': 'en',
-                        'text': name
-                    }
-                });
-                ces.push(name);
-            }
+        SMTElectionVote.updateState({skillId: $stateParams.skillId}, {smt_election_state: 'IN_PROGRESS'}, function () {
+            $scope.skill.smt_election_state = 'IN_PROGRESS';
         });
-
-        $translate('message_confirm_create_poll', {experts: ces.join(', ')}).then(function (message) {
-            if (confirm(message)) {
-        
-                var poll = {
-                    start: new Date(),
-                    expiry: new Date(),
-                    entity: {
-                        id: $scope.skill.entity_id
-                    },
-                    title: { lang_code: 'en', text: 'Chief Expert election - ' + $scope.skill.name.text },
-                    question: { lang_code: 'en', text: 'Please select your choices for the Chief Expert position in order of preference:' },
-                    type: 'weighted',
-                    numberOfSelections: numberOfSelections,
-                    anonymousResults: true,
-                    anonymousVoting: false,
-                    showingResults: false,
-                    allowingReVote: true,
-                    allowingAbstain: false,
-                    whitelist: true,
-                    options: options,
-                    allowedVoters: experts
-                };
-
-                // C+1 14:00, see Competitions Rules 6.5.5 Nomination, election, and approval
-                poll.expiry.setDate(poll.expiry.getDate() + 1);
-                poll.expiry.setHours(14);
-                poll.expiry.setMinutes(0);
-                poll.expiry.setSeconds(0);
-
-                var p = Poll.save(poll, function (response) {
-                    $translate('message_poll_created').then(function (message) {
-                        alert.success(message);
-                        $state.go('skill', {eventId: $scope.skill.event.id, skillId: $scope.skill.id});
-                    });
-                }, function (response) {
-                    window.alert('An error has occured: ' + JSON.stringify(response.data));
-                });
-            }
+    };
+    $scope.closePoll = function () {
+        SMTElectionVote.updateState({skillId: $stateParams.skillId}, {smt_election_state: 'COMPLETED'}, function () {
+            $scope.skill.smt_election_state = 'COMPLETED';
         });
     };
 
--- app/scripts/services/SMTElectionVote.js
+++ app/scripts/services/SMTElectionVote.js
@@ -5,11 +5,18 @@
 
         return $resource(WORLDSKILLS_API_SKILLMAN + '/skills/:skillId/smt_election_votes/:voterId', {
         }, {
+            query: {
+                method: 'GET'
+            },
             get: {
                 method: 'GET'
             },
             update: {
                 method: 'PUT'
+            },
+            updateState: {
+                method: 'PUT',
+                url: WORLDSKILLS_API_SKILLMAN + '/skills/:skillId/smt_election_state'
             }
         });
 
